1b390f1f710d5aa8ee633bbc145c9927d42fefbf
815382636 <815382636@qq.com>
2024-03-08 11:00:31 +0800

    构建prompt

--- dataset/ml-1m/process.py
+++ dataset/ml-1m/process.py
@@ -1,9 +1,9 @@
-import numpy as np
 import pandas as pd
+import copy
 
 
 def get_ratings(file):
-    header = ["user_id", "item_id", "rating", "timestamp"]
+    header = ["userID", "itemID", "rating", "timestamp"]
     data = pd.read_csv(file, sep="::", names=header, engine="python")
     return data
 
@@ -14,7 +14,7 @@ def get_items(file):
         engine="python",
         sep="::",
         encoding="ISO-8859-1",
-        names=["item_id", "title", "genres"],
+        names=["itemID", "title", "genres"],
     )
     return data
 
@@ -25,7 +25,7 @@ def get_users(file):
         engine="python",
         sep="::",
         encoding="ISO-8859-1",
-        names=["user_id", "gender", "age", "occupation", "zip_code"],
+        names=["userID", "gender", "age", "occupation", "zip_code"],
     )
 
     def occupations_map(occupation):
@@ -65,6 +65,189 @@ def get_users(file):
     return data
 
 
+class Dataset(object):
+    def __init__(self, dataset, u2i=True):
+        self.dataset = dataset
+        self.use_u2i = u2i
+        self.n_users = self.dataset["userID"].nunique()
+        self.n_items = self.dataset["itemID"].nunique()
+
+    def process_data(
+        self,
+        threshold=4,
+        order=True,
+        leave_n=1,
+        keep_n=5,
+        max_history_length=5,
+        premise_threshold=0,
+    ):
+        # filter ratings by threshold
+        self.proc_dataset = self.dataset.copy()
+        # self.proc_dataset["rating"][self.proc_dataset["rating"] < threshold] = 0
+        # self.proc_dataset["rating"][self.proc_dataset["rating"] >= threshold] = 1
+        if order:
+            self.proc_dataset = self.proc_dataset.sort_values(
+                by=["timestamp", "userID", "itemID"]
+            ).reset_index(drop=True)
+
+        self.leave_one_out_by_time(leave_n, keep_n)
+        self.generate_histories(max_hist_length=max_history_length, premise_threshold=0)
+
+    def leave_one_out_by_time(self, leave_n=1, keep_n=5):
+        train_set = []
+        # generate training set by looking for the first keep_n POSITIVE interactions
+        processed_data = self.proc_dataset.copy()
+        for uid, group in processed_data.groupby("userID"):  # group by uid
+            found, found_idx = 0, -1
+            for idx in group.index:
+                if group.loc[idx, "rating"] > 3:
+                    found_idx = idx
+                    found += 1
+                    if found >= keep_n:
+                        break
+            if found_idx > 0:
+                train_set.append(group.loc[:found_idx])
+        train_set = pd.concat(train_set)
+        # drop the training data info
+        processed_data = processed_data.drop(train_set.index)
+
+        # generate test set by looking for the last leave_n POSITIVE interactions
+        test_set = []
+        for uid, group in processed_data.groupby("userID"):
+            found, found_idx = 0, -1
+            for idx in reversed(group.index):
+                if group.loc[idx, "rating"] > 3:
+                    found_idx = idx
+                    found += 1
+                    if found >= leave_n:
+                        break
+            if found_idx > 0:
+                test_set.append(group.loc[found_idx:])
+        test_set = pd.concat(test_set)
+        processed_data = processed_data.drop(test_set.index)
+
+        validation_set = []
+        for uid, group in processed_data.groupby("userID"):
+            found, found_idx = 0, -1
+            for idx in reversed(group.index):
+                if group.loc[idx, "rating"] > 3:
+                    found_idx = idx
+                    found += 1
+                    if found >= leave_n:
+                        break
+            # put all the negative interactions encountered during the search process into validation set
+            if found_idx > 0:
+                validation_set.append(group.loc[found_idx:])
+        validation_set = pd.concat(validation_set)
+        processed_data = processed_data.drop(validation_set.index)
+
+        # The remaining data (after removing validation and test) are all in training data
+        self.train_set = pd.concat([train_set, processed_data])
+        self.validation_set, self.test_set = validation_set.reset_index(
+            drop=True
+        ), test_set.reset_index(drop=True)
+
+    def generate_histories(self, max_hist_length=5, premise_threshold=0):
+        # TODO insert an assert to check the value of the parameter premise_threshold (read doc)
+        history_dict = (
+            {}
+        )  # it contains for each user the list of all the items he has seen
+        feedback_dict = (
+            {}
+        )  # it contains for each user the list of feedbacks he gave to the items he has seen
+        for df in [self.train_set, self.validation_set, self.test_set]:
+            history = (
+                []
+            )  # each element of this list is a list containing the history items of a single interaction
+            fb = (
+                []
+            )  # each element of this list is a list containing the feedback for the history items of a
+            # single interaction
+            hist_len = (
+                []
+            )  # each element of this list indicates the number of history items of a single interaction
+            type = []  # 2表示u2i节点， 1表示i2u
+            uids, iids, feedbacks = (
+                df["userID"].tolist(),
+                df["itemID"].tolist(),
+                df["rating"].tolist(),
+            )
+            for i, uid in enumerate(uids):
+                iid, feedback = iids[i], feedbacks[i]
+
+                if uid not in history_dict:
+                    history_dict[uid] = []
+                    feedback_dict[uid] = []
+
+                # list containing the history for current interaction
+                tmp_his = (
+                    copy.deepcopy(history_dict[uid])
+                    if max_hist_length == 0
+                    else history_dict[uid][-max_hist_length:]
+                )
+                # list containing the feedbacks for the history of current interaction
+                fb_his = (
+                    copy.deepcopy(feedback_dict[uid])
+                    if max_hist_length == 0
+                    else feedback_dict[uid][-max_hist_length:]
+                )
+
+                history.append(tmp_his)
+                fb.append(fb_his)
+                hist_len.append(len(tmp_his))
+                if tmp_his == []:
+                    type.append(2)
+                else:
+                    type.append(4)
+                history_dict[uid].append(iid)
+                feedback_dict[uid].append(feedback)
+
+            df["history"] = history
+            df["history_feedback"] = fb
+            df["history_length"] = hist_len
+            df["type"] = type
+
+        if premise_threshold != 0:
+            self.train_set = self.train_set[
+                self.train_set.history_length > premise_threshold
+            ]
+            self.validation_set = self.validation_set[
+                self.validation_set.history_length > premise_threshold
+            ]
+            self.test_set = self.test_set[
+                self.test_set.history_length > premise_threshold
+            ]
+
+        self.clean_data()
+
+    def clean_data(self):
+        train_type1 = self.train_set.copy()
+        train_type1["type"] = 1
+        type1 = train_type1.sample(frac=1.0, replace=False, random_state=2022, axis=0)
+        self.train_set = self.train_set[self.train_set["rating"] > 3].reset_index(
+            drop=True
+        )
+        self.train_set = self.train_set[
+            self.train_set["history_feedback"].map(len) >= 1
+        ].reset_index(drop=True)
+        self.validation_set = self.validation_set[
+            self.validation_set["rating"] > 3
+        ].reset_index(drop=True)
+        self.validation_set = self.validation_set[
+            self.validation_set["history_feedback"].map(len) >= 1
+        ].reset_index(drop=True)
+        self.test_set = self.test_set[self.test_set["rating"] > 3].reset_index(
+            drop=True
+        )
+        self.test_set = self.test_set[
+            self.test_set["history_feedback"].map(len) >= 1
+        ].reset_index(drop=True)
+        if self.use_u2i:
+            self.train_set = pd.concat(
+                [type1, self.train_set], axis=0, ignore_index=True
+            )
+
+
 if __name__ == "__main__":
 
     file = "ratings.dat"
@@ -78,3 +261,30 @@ if __name__ == "__main__":
     file = "users.dat"
     user_data = get_users(file)
     print(user_data)
+
+    data = Dataset(rating_data)
+    data.process_data()
+    print(data.train_set)
+
+    gender = ""
+    age = ""
+    occupation = ""
+    item1 = ""
+    star1 = ""
+    item2 = ""
+    star2 = ""
+    item3 = ""
+    star3 = ""
+    item4 = ""
+    star4 = ""
+    item5 = ""
+    star5 = ""
+    item6 = ""
+    star6 = ""
+
+    prompt = f"""
+        Given a user who is '{gender}, {age} years old, and occupation is {occupation}', his (her) movie viewing history over time includes: '{item1}, {star1} star; {item2}, {star2} star; {item3}, {star3} star; {item4}, {star4} star; {item5}, {star5} star;'
+    """
+    cot_prompt = f"""
+        He (she) will watch {item6} next, please provide clear explanations based on details from the user's viewing history and other pertinent factors.
+    """
